3a58a0e99461e6a355979058fda1ada7159070fe
seduocuc2024 <se.penap@duocuc.cl>
2024-11-14 23:41:12 -0400

    Se agrega controller de registro receta

--- src/main/java/com/appweb/recetas/config/Constants.java
+++ src/main/java/com/appweb/recetas/config/Constants.java
@@ -1,8 +1,8 @@
 package com.appweb.recetas.config;
 
 public class Constants {
-    //public static final String BACKEND_URL = "http://localhost:8084";
-    public static final String BACKEND_URL = "http://52.251.94.76:8084";
+    public static final String BACKEND_URL = "http://localhost:8084";
+    //public static final String BACKEND_URL = "http://52.251.94.76:8084";
     public static final String BACKEND_URL_AUTH = "/api/authentication/login";
     public static final String SECRET_KEY = "#E@M%R!&EGD%srHG823sAK3&gBi&U4Q7"; // Clave secreta para firmar el token
 }
