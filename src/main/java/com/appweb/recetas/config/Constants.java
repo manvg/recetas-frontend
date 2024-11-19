9d6a1b0b5ac98e22b9c64c8818ec5dcf8944eca0
Manuel Valdés <manuelvaldesg97@gmail.com>
2024-11-18 22:21:30 -0300

    Cambio a URL productiva

--- src/main/java/com/appweb/recetas/config/Constants.java
+++ src/main/java/com/appweb/recetas/config/Constants.java
@@ -1,8 +1,8 @@
 package com.appweb.recetas.config;
 
 public class Constants {
-    public static final String BACKEND_URL = "http://localhost:8084";
-    //public static final String BACKEND_URL = "http://52.251.94.76:8084";
+    //public static final String BACKEND_URL = "http://localhost:8084";
+    public static final String BACKEND_URL = "http://52.251.94.76:8084";
     public static final String BACKEND_URL_AUTH = "/api/authentication/login";
     public static final String ABSOLUTE_PATH_VIDEOS = "src/main/resources/static/videos";
     public static final String ABSOLUTE_PATH_IMAGES = "src/main/resources/static/images";
